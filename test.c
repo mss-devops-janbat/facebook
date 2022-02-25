534f961dd1f9d8376116975a861b90a5e728860b
Basheer Mubarak <basheer.biminfo@gmail.com>
2022-02-25 16:05:26 +0530

    new files

---
+++ test.c
@@ -0,0 +1,8 @@
+hasbhbn Zx
+ HZxhjna
+ n ZHJ CJs'
+  NZCXjNZX
+   JZXC jz
+   z xj jz 
+   zxcjnxkcm
+   zxc jkxnkc
---
+++ test.sh
@@ -0,0 +1,7 @@
+nzhjxcjs hd
+m zxjncjs
+m zxcj 
+ cjk s
+  scdj jw
+   nj cjs
+    cj s
