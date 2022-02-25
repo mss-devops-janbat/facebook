c01ae21af19615c70dd77cdcb4d7746582bd74c0
Basheer Mubarak <basheer.biminfo@gmail.com>
2022-02-25 16:07:44 +0530

    deleted...Revert "new files"
    This reverts commit 534f961dd1f9d8376116975a861b90a5e728860b.

--- test.c
+++
@@ -1,8 +0,0 @@
-hasbhbn Zx
- HZxhjna
- n ZHJ CJs'
-  NZCXjNZX
-   JZXC jz
-   z xj jz 
-   zxcjnxkcm
-   zxc jkxnkc
--- test.sh
+++
@@ -1,7 +0,0 @@
-nzhjxcjs hd
-m zxjncjs
-m zxcj 
- cjk s
-  scdj jw
-   nj cjs
-    cj s
