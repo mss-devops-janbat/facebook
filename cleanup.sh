3312030f23585ec5e7022a3894d43ce13ef3b8f5
Basheer Mubarak <basheer.biminfo@gmail.com>
2022-02-28 14:32:26 +0530

    updated

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 this is master brach which i have to updated
 this is Developement branch which i have to updated..
-this script deletes the unused files in  temp Directory
+this script deletes the unused files in  temp Directory		
+hi this is updated file for checking stash command
